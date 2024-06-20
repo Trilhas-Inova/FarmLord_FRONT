06e9ce111ae876a2e5df61af6e373d62783b08ca
Marcew44 <120995295+Marcew44@users.noreply.github.com>
2024-06-20 12:40:05 -0300

    Arquivo base do projeto
    Seguintes mudanças foram feitas: HTML foi desenvolvido, adicionado um script JS para o botão "voltar para o topo" e estilização do HTML com o CSS.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// script para o botão de voltar ao topo
+const backToTopButton = document.getElementById('scrollbuttonid');
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.scrollY > 200) { // Show backToTopButton
+    if(!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else { // Hide backToTopButton
+    if(backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
